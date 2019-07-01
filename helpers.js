7096c98a6bff645ae12157fd6bf1a28000af2840
Ulysse McConnell <ulysse.mcconnell+dev@protonmail.com>
2019-07-01 18:15:09 +0200

    Add plus helper

---
+++ helpers.js
@@ -0,0 +1,11 @@
+/** @module leaf-detective/helpers */
+
+/**
+ * Add two numbers
+ * @param {(Number|String)} a first number
+ * @param {(Number|String)} b second number
+ * @returns {Number} sum of both numbers
+ */
+export function plus(a, b) {
+    return a + b;
+}
